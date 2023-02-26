410388454d86cb5588a584af1c7adcb14ad125c2
Reno <101154856+RenoRenoRenoReno@users.noreply.github.com>
2023-02-25 19:46:14 -0500

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,19 @@
+for _,User in pairs(game.workspace:GetChildren()) do
+	if (User:FindFirstChild("TPVAccessoryMagpack2L")) then
+	   local highlight = Instance.new("Highlight", User)
+	   if (_G.EnemyColor == 'default' or _G.EnemyColor == 'Default' ) then
+	   	      print("Not changing ENEMY COLOR ESP")
+	      else
+	          highlight.FillColor = _G.EnemyColor	
+	   end
+	   if (User:FindFirstChild("friendly_marker")) then
+	   	  if (User['friendly_marker']:FindFirstChild('name_label')) then
+	   	  	 if (_G.TeamColorEnabled == false) then
+	   	  	    print("Found teammate, not changing ESP Color")
+		     end
+	   	     print("Found friendly user. Changing ESP color.")
+	   	     highlight.FillColor = _G.TeamColor
+		  end
+	   end
+	end
+end
